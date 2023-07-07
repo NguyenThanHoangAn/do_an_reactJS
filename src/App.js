927484e74fe184f6969ad564fee1836e8b49149f
Thanh Tai <tai.btpk@gmail.com>
2023-07-07 23:41:03 +0700

    add file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 
 import './App.css';
 import Header from "./common/header/Header";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Pages from './pages/Pages';
 import Data from "./components/flashDeals/Data"
 import Sdata from "./components/shop/Sdata"
 import { useState } from 'react';
 import Cart from "./common/Cart/Cart";
+import Footer from './common/footer/Footer';
 
 
 function App() {
 
-  const { productItems} = Data
+  const { productItems } = Data
   const { shopItems } = Sdata
   const [CartItem, setCartItem] = useState([])
 
@@ -19,30 +20,32 @@ function App() {
     const productExit = CartItem.find((item) => item.id === product.id)
     if (productExit) {
       setCartItem(CartItem.map((item) => (item.id === product.id ? { ...productExit, qty: productExit.qty + 1 } : item)))
-    } else {setCartItem([...CartItem, { ...product, qty: 1 }])
+    } else {
+      setCartItem([...CartItem, { ...product, qty: 1 }])
+    }
   }
-}
-const decreaseQty = (product) => {
-  const productExit = CartItem.find((item) => item.id === product.id)
-  if (productExit.qty === 1) {
-    setCartItem(CartItem.filter((item) => item.id !== product.id))
-  } else {
-    setCartItem(CartItem.map((item) => (item.id === product.id ? { ...productExit, qty: productExit.qty - 1 } : item)))
+  const decreaseQty = (product) => {
+    const productExit = CartItem.find((item) => item.id === product.id)
+    if (productExit.qty === 1) {
+      setCartItem(CartItem.filter((item) => item.id !== product.id))
+    } else {
+      setCartItem(CartItem.map((item) => (item.id === product.id ? { ...productExit, qty: productExit.qty - 1 } : item)))
+    }
   }
-}
   return (
-   
-     <>
-     <Router>
-        <Header CartItem={CartItem}/>
+
+    <>
+      <Router>
+        <Header CartItem={CartItem} />
         <Switch>
           <Route path='/' exact>
-            <Pages productItems={productItems} addToCart={addToCard} shopItems={shopItems}/>
+            <Pages productItems={productItems} addToCart={addToCard} shopItems={shopItems} />
           </Route>
           <Route path='/cart' exact>
-          <Cart CartItem={CartItem} addToCart={addToCard} decreaseQty={decreaseQty} />
+            <Cart CartItem={CartItem} addToCart={addToCard} decreaseQty={decreaseQty} />
           </Route>
         </Switch>
+        <Footer />
       </Router>
     </>
   );
