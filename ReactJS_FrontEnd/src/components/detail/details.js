23d262cc89d13501cd5c678809eb09b85199ed45
nguyenTrungKienn <tk624019@gmail.com>
2023-08-11 15:22:16 +0700

    up detail

--- ReactJS_FrontEnd/src/components/detail/details.js
+++ ReactJS_FrontEnd/src/components/detail/details.js
@@ -1,14 +1,44 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "./details.css";
+import Header from "../../common/header/Header";
+import Footer from "../../common/footer/Footer"
 
 const Details = () => {
+  const [quantity, setQuantity] = useState (1)
+
+  const images = [
+    "./images/shops/shops-1.png",
+    "./images/shops/shops-2.png"
+  ]
+  
     return (
         <>
-      <div>
-        Kien beo
+        <Header/>
+      <div className="pd">
+        <div className="left">
+          <div className="images">
+            <img src={images[0]} alt=""/>
+          </div>
+        </div>
+        <div className="right">
+          <h1>Azzaro Wanted</h1>
+          <span className="price">180.000 VNĐ</span>
+          <p>
+          Bạn đang đi tìm một mùi hương dễ dùng, đa dụng, nhưng không quá quen thuộc theo kiểu những chai nước hoa phổ biến? Azzaro Wanted sẽ là một lựa chọn thú vị nhưng không kém an toàn dành cho bạn, bằng hương thơm theo kiểu Gỗ - Gia vị ấm áp, được hoàn thiện chỉn chu cả về mùi hương và ngoại hình với một chiếc chai đựng bắt mắt.
+          </p>
+          <div className="quantily">
+            <button onClick={()=>setQuantity((prev)=>(prev === 1 ? 1 : prev -1))}>-</button>
+            {quantity}
+            <button onClick={()=>setQuantity(prev=>prev+1)}>+</button>
+          </div>
+          <button className="add">
+            <addToCart/> Thêm vào giỏ hàng
+          </button>
+        </div>
       </div>
-        
+        <Footer/>
         </>
     )
 }
+
 export default Details;
--- ReactJS_FrontEnd/src/components/shop/Sdata.js
+++ ReactJS_FrontEnd/src/components/shop/Sdata.js
@@ -3,7 +3,7 @@ const Sdata = {
       {
         id: 7,
         cover: "./images/shops/shops-1.png",
-        name: "MÙI HOA LÀI",
+        name: "Azzaro Wanted",
         price: "180",
         discount: "25",
       },
